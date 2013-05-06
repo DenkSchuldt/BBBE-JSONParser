cd39bd5d314c8ca5387afae0faf86686233551df
Denny K. Schuldt <dschuldt@espol.edu.ec>
2013-05-06 17:30:21 -0500

    Paring JSON to XML

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -4,12 +4,25 @@
  */
 package Logic;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
+import org.json.JSONObject;
 
 /**
  *
@@ -17,22 +30,39 @@ import javax.swing.JOptionPane;
  */
 public class Logic{
         
-    public static ResultSet result = null;        
+    public static ResultSet result = null;    
     public static Connection connection;
     public static Statement query_statement;
     public static String path;
     
-    public static void main(String args[]){
-        path = "C:/Users/Denny/Desktop/popcorn.sqlite";
+    public static void main(String args[]) throws IOException{
         
-        String sql = "select * from Projects";
-        try {
+        path = "popcorn.sqlite";
+        String sql = "select * from Projects"; // + Email.
+        
+        try{
             result = query(sql);
             if(result != null){
                 int number_columns = result.getMetaData().getColumnCount();
                 for(int j = 1;j <= number_columns;j++){
-                    System.out.println(""+result.getMetaData().getColumnName(j));                    
-                }                
+                    System.out.println(""+result.getMetaData().getColumnName(j));
+                }
+                                
+                FileWriter fw = new FileWriter("json.json");
+                PrintWriter pw = new PrintWriter(fw);
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                JsonParser jp = new JsonParser();
+                JsonElement je = jp.parse(result.getString(2));
+                String prettyJsonString = gson.toJson(je);
+                pw.println(prettyJsonString);
+                fw.close();
+                                
+                JSONObject jo = new JSONObject(readFile("json.json"));                
+                String xml = org.json.XML.toString(jo);
+                FileWriter fw2 = new FileWriter("generated.xml");
+                PrintWriter pw2 = new PrintWriter(fw2);
+                pw2.println(xml);
+                fw2.close();
             }
         }catch(SQLException e){}
         finally{
@@ -41,7 +71,7 @@ public class Logic{
                 connection.close();
                 if(result != null){
                     result.close();
-                 }
+                }
             }
             catch (Exception e){
                 e.printStackTrace();
@@ -58,7 +88,7 @@ public class Logic{
             System.out.println("Message: " + e.getMessage());
             System.out.println("Status: " + e.getSQLState());
             System.out.println("Error code: " + e.getErrorCode());
-            JOptionPane.showMessageDialog(null,""+e.getMessage());
+            JOptionPane.showMessageDialog(null,"0 -> "+e.getMessage());
         }
         return result;
     }    
@@ -68,14 +98,25 @@ public class Logic{
             Class.forName("org.sqlite.JDBC");
         }
         catch (ClassNotFoundException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
+            JOptionPane.showMessageDialog(null, "1 -> " + e.getMessage());
         }	 
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:" + path);
             query_statement = connection.createStatement();
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
+            JOptionPane.showMessageDialog(null,"2 -> "  + e.getMessage());
         }
-    }    
+    }
     
+    private static String readFile(String path) throws IOException {
+        FileInputStream stream = new FileInputStream(new File(path));
+        try {
+            FileChannel fc = stream.getChannel();
+            MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            return Charset.defaultCharset().decode(bb).toString();
+        }
+        finally {
+            stream.close();
+        }
+    }
 }
